b889288965e51a728991937d21cb93a32ed96b35
Gabriel Simonetti Marconato <gabrielsmarconato@gmail.com>
2025-05-30 22:58:10 -0300

    Adição de mais estrutura

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="w-full fixed bg-red-600 text-white text-lg font-bold">
+        <p>{{ new Date().getFullYear() }}</p>
+        <p>(14)99696-0923</p>
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import {creatRouter, createWebHistory} from 'vue-router'
+
+const router = creatRouter({
+    history: creatRouter(import.meta.env.BASE_URL),
+    routes:[
+
+    ]
+})
+export default router
